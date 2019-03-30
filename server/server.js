a7c21810b94c5fba122c364e22b48d6bed262310
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-03-29 18:05:12 -0600

    Required mongoose bodyParser and cookieParser

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const app = express();
+const mongoose = require('mongoose');
 
 const port = process.env.PORT || 3002;
 
