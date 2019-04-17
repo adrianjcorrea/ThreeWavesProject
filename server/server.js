3f4cb677373b9e0c6998820fe88ff72f328a2d19
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-16 23:54:24 -0500

    Added authentication endpoint

--- server/server.js
+++ server/server.js
@@ -15,11 +15,27 @@ app.use(cookieParser());
 
 //Models
 const { User } = require('./models/users');
+
+//Middlewares
+const { auth } = require('./middleware/auth');
 //===========================================
 //                  USERS                  ==
 //===========================================
+app.get('/api/users/auth',auth ,(req, res) => {
+     res.status(200).json({
+         isAdmin: req.user.role === 0 ? false : true,
+         isAuth: true,
+         email: req.user.email,
+         name: req.user.name,
+         lastname: req.user.lastname,
+         role: req.user.role,
+         cart: req.user.cart
+         history: req.user.history
+     })
+})
 
-app.post('/api/users/register',(req,res)=>{
+
+app.post('/api/users/register',(req,res) => {
   //The req.body defines the new user.
   const user = new User(req.body);
 
@@ -29,8 +45,8 @@ app.post('/api/users/register',(req,res)=>{
     if(err) return res.json({success:false,err});
     //Or success true and the document from MongoDB.
     res.status(200).json({
-      success:true,
-      userData: doc
+      success:true
+    //  userData: doc
     })
   })
 })
