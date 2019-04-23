41a9ed09f41e4aa1ffe3351dc112950c70dbf899
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-23 18:49:31 -0500

    Added Header and Footer

--- client/src/components/hoc/Layout.js
+++ client/src/components/hoc/Layout.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Header from '../header&footer/Header.js';
+import Footer from '../header&footer/Footer.js';
 
 class Layout extends Component {
   render() {
     return (
       <div>
-        Header Here
+        <Header />
         <div className='page_container'>
             {this.props.children}
         </div>
-        Footer here
+        <Footer />
       </div>
     )
   }
