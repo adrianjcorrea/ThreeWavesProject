2e57ffcf400296b22fcf5fc1b4a1a3be38cdec7c
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-23 15:15:50 -0500

    added proxy config to use threeWaves api

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        Three waves Client
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+    axios.get('/api/products/getBrands').then(response => {
+      console.log(response);
+    });
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          Three waves Client
+        </header>
+      </div>
+    );
+  }  
 }
 
 export default App;
