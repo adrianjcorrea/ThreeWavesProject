1686c17f99637981aa266c855d70b1ba65c7135f
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-23 17:06:02 -0500

    Create Home component add it as main route

--- client/src/Routes.js
+++ client/src/Routes.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import Home from './components/home/Home.js';
 const Routes = () => {
 
   return (
     <div>
       <Switch>
-        <Route />
+        <Route path='/' component={ Home } />
         <Route />
       </Switch>
     </div>
