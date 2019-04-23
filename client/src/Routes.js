f4be637f4d73e76152126f2c386c4ea1d667e58e
adrianjcorrea <adrianjcorrea007@gmail.com>
2019-04-23 16:55:18 -0500

    Imported properties from react-router-dom

---
+++ client/src/Routes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+const Routes = () => {
+
+  return (
+    <div>
+      <Switch>
+        <Route />
+        <Route />
+      </Switch>
+    </div>
+  );
+}  
+
+export default Routes;
